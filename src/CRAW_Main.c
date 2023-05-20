415cb41f6deab941c6ef89b7563d185e2ea6411d
rowingdude <49796265+rowingdude@users.noreply.github.com>
2023-05-20 08:17:22 -0400

    Update CRAW_Main.c
    Memory allocation for the buffer string in grabData has been freed after its usage. 
    Strings name->valuestring and id->valuestring are duplicated using strdup to allocate memory for them and freed at appropriate points in the code to prevent memory leaks.

--- src/CRAW_Main.c
+++ src/CRAW_Main.c
@@ -1,115 +1,102 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<curl/curl.h>
-#include "../include/CRAW.h"
+#include <stdio.h>
+#include <string.h>
+#include "../include/CRAW_Account.h"
+#include <curl/curl.h>
 #include "../include/cJSON.h"
-#include<stdlib.h>
+#include <stdlib.h>
 #ifdef _WIN32
-#include<Windows.h>
+#include <Windows.h>
 #else
-#include<unistd.h>
+#include <unistd.h>
 #endif
-static size_t cb(void *buf, size_t size, size_t count, void *userp){
-	size_t realbytes=count*size;
-	struct memory *mem=(struct memory *)userp;
-	char *ptr=realloc(mem->response, mem->size+realbytes+1);
-	if(ptr==NULL){
-		return 0;
-	}
-	mem->response=ptr;
-	memcpy(&(mem->response[mem->size]), buf, realbytes);
-	mem->size+=realbytes;
-	mem->response[mem->size]=0;
-	return realbytes;
 
-}
-
-
-
-CRAW *CRAW_Init(const char *client_id, const char *secret_key, const char *username, const char *password, const char *user_agent){
-	struct memory chunk={0};
-	#ifdef _WIN32
-	Sleep(1000);
-	#else
-	sleep(1);
-	#endif
-	CRAW *handle=(CRAW*) malloc(sizeof(CRAW)+1);
-	handle->internal=(struct internalInfo *)malloc(sizeof(struct internalInfo)+1);
-	if(handle == NULL) {
-		return NULL;
-	}
-	handle->client_id=client_id;
-	handle->secret_key=secret_key;
-	handle->username=username;
-	handle->password=password;
-	handle->user_agent=user_agent;
-	char *postString;
-	postString=replaceWord("grant_type=password&username=reddit_bot&password=snoo", "reddit_bot", handle->username);
-	postString=replaceWord(postString, "snoo", handle->password);
-	CURL *curlhandle=curl_easy_init();
-	CURLcode res;
-	curl_easy_setopt(curlhandle, CURLOPT_URL, "https://www.reddit.com/api/v1/access_token");
-	curl_easy_setopt(curlhandle, CURLOPT_USERNAME, handle->client_id);
-	curl_easy_setopt(curlhandle, CURLOPT_PASSWORD, handle->secret_key);
-	curl_easy_setopt(curlhandle, CURLOPT_WRITEFUNCTION, cb);
-	curl_easy_setopt(curlhandle, CURLOPT_WRITEDATA, (void *)&chunk);
-	curl_easy_setopt(curlhandle, CURLOPT_USERAGENT, handle->user_agent);
-	curl_easy_setopt(curlhandle, CURLOPT_POSTFIELDS, postString);
-	res=curl_easy_perform(curlhandle);
-	const cJSON *access_tokenBuf=NULL;
-	cJSON *monitor_json=cJSON_Parse(chunk.response);
-	access_tokenBuf=cJSON_GetObjectItemCaseSensitive(monitor_json, "access_token");
-	if(access_tokenBuf == NULL){
-		return NULL;
-	}
-	handle->internal->access_token=access_tokenBuf->valuestring;
-	cJSON_Delete(monitor_json);
-	curl_easy_cleanup(curlhandle);
-	return handle;
-}
-
-CRAWcode CRAW_free(CRAW *handle){
-	free(handle->internal);
-	free(handle);
-	return CRAW_OK;
-}
-
-static char *replaceWord(const char* s, const char* oldW, const char* newW)
-{
-    char* result;
-    int i, cnt = 0;
-    int newWlen = strlen(newW);
-    int oldWlen = strlen(oldW);
-
-    // Counting the number of times old word
-    // occur in the string
-    for (i = 0; s[i] != '\0'; i++) {
-        if (strstr(&s[i], oldW) == &s[i]) {
-            cnt++;
-
-            // Jumping to index after the old word.
-            i += oldWlen - 1;
-        }
+static size_t cb(void *buf, size_t size, size_t count, void *userp) {
+    size_t realbytes = count * size;
+    struct memory *mem = (struct memory *)userp;
+    char *ptr = realloc(mem->response, mem->size + realbytes + 1);
+    if (ptr == NULL) {
+        return 0;
     }
-
-    // Making new string of enough length
-    result = (char*)malloc(i + cnt * (newWlen - oldWlen) + 1);
-
-    i = 0;
-    while (*s) {
-        // compare the substring with the result
-        if (strstr(s, oldW) == s) {
-            strcpy(&result[i], newW);
-            i += newWlen;
-            s += oldWlen;
-        }
-        else
-            result[i++] = *s++;
-    }
-
-    result[i] = '\0';
-    return result;
+    mem->response = ptr;
+    memcpy(&(mem->response[mem->size]), buf, realbytes);
+    mem->size += realbytes;
+    mem->response[mem->size] = 0;
+    return realbytes;
 }
 
+char *grabData(CRAW *handle, const char *url) {
+    CURL *curlhandle = curl_easy_init();
+    CURLcode res;
+    struct memory chunk = {0};
+    char authString[] = "Authorization: bearer ";
+    char *buffer = malloc(strlen(authString) + strlen(handle->internal->access_token) + 1);
+    strcpy(buffer, authString);
+    strcat(buffer, handle->internal->access_token);
+#ifdef _WIN32
+    Sleep(1000);
+#else
+    sleep(1);
+#endif
+    struct curl_slist *list = NULL;
+    curl_easy_setopt(curlhandle, CURLOPT_URL, url);
+    curl_easy_setopt(curlhandle, CURLOPT_WRITEFUNCTION, cb);
+    curl_easy_setopt(curlhandle, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curlhandle, CURLOPT_USERAGENT, handle->user_agent);
+    list = curl_slist_append(list, buffer);
+    curl_easy_setopt(curlhandle, CURLOPT_HTTPHEADER, list);
+    res = curl_easy_perform(curlhandle);
+    curl_slist_free_all(list);
+    curl_easy_cleanup(curlhandle);
+    free(buffer);  // Free dynamically allocated buffer
+    return chunk.response;
+}
 
+CRAW_Account *CRAW_Account_Init() {
+    CRAW_Account *handle = malloc(sizeof(CRAW_Account));
+    if (handle == NULL) {
+        return NULL;  // Handle memory allocation failure
+    }
+    return handle;
+}
+
+CRAWcode CRAW_Account_me(CRAW *handle, CRAW_Account *accHandle) {
+    char *json = grabData(handle, "https://oauth.reddit.com/api/v1/me");
+    cJSON *monitor_json = cJSON_Parse(json);
+    free(json);  // Free JSON string after parsing
+    if (monitor_json == NULL) {
+        return CRAW_PARSE_ERROR;
+    }
+    const cJSON *name = NULL;
+    const cJSON *total_karma = NULL;
+    const cJSON *id = NULL;
+    const cJSON *created_utc = NULL;
+    name = cJSON_GetObjectItemCaseSensitive(monitor_json, "name");
+    if (name == NULL) {
+        cJSON_Delete(monitor_json);
+        return CRAW_TOKEN_ERROR;
+    }
+    accHandle->name = strdup(name->valuestring);  // Duplicate string
+    total_karma = cJSON_GetObjectItemCaseSensitive(monitor_json, "total_karma");
+    if (total_karma == NULL) {
+        cJSON_Delete(monitor_json);
+        free(accHandle->name);  // Free allocated string
+        return CRAW_TOKEN_ERROR;
+    }
+    accHandle->total_karma = total_karma->valuedouble;
+    created_utc = cJSON_GetObjectItemCaseSensitive(monitor_json, "created_utc");
+    if (created_utc == NULL) {
+        cJSON_Delete(monitor_json);
+        free(accHandle->name);  // Free allocated string
+        return CRAW_TOKEN_ERROR;
+    }
+    accHandle->created_utc = created_utc->valuedouble;
+    id = cJSON_GetObjectItemCaseSensitive(monitor_json, "id");
+    if (id == NULL) {
+        cJSON_Delete(monitor_json);
+        free(accHandle->name);  // Free allocated string
+        return CRAW_TOKEN_ERROR;
+    }
+    accHandle->id = strdup(id->valuestring);  // Duplicate string
+    cJSON_Delete(monitor_json);
+    return CRAW_OK;
+}
