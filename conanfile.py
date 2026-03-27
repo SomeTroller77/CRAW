b0798df05744d81e71355ff7863228e3793575cc
ericLemanissier <ericLemanissier@users.noreply.github.com>
2026-03-27 10:31:44 +0100

    Add GitHub Actions workflow for Conan CI

--- conanfile.py
+++ conanfile.py
@@ -1,38 +1,58 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.files import get, copy
-import os
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.gnu import PkgConfigDeps
 
 
 class CrawConan(ConanFile):
     name = "craw"
     description = "C Reddit API wrapper"
     license = "MIT"
-    url = "https://github.com/YOUR_USERNAME/craw"
-    homepage = "https://github.com/YOUR_USERNAME/craw"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/SomeTroller77/CRAW"
     topics = ("reddit", "api", "c")
 
     package_type = "library"
 
     settings = "os", "arch", "compiler", "build_type"
 
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    implements = ["auto_shared_fpic"]
 
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
 
-    generators = "CMakeToolchain", "CMakeDeps"
-
-    requires = (
-        "libcurl/8.18.0"
-        "cjson/1.7.17",
-    )
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
         cmake_layout(self)
 
-    def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+    def build_requirements(self):
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
+
+    def requirements(self):
+        self.requires("libcurl/8.18.0")
+        self.requires("cjson/1.7.17")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = PkgConfigDeps(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
